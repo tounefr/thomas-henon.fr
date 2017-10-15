f9f7c016e7deb5c6131e016ba00ac5cb648e4b79
Thomas HENON <thomas.henon@epitech.eu>
2017-10-15 10:32:25 +0200

    analytics + cv

--- app/web/front/assets/js/analytics.js
+++ app/web/front/assets/js/analytics.js
@@ -1,11 +1,11 @@
 var _paq = _paq || [];
-_paq.push(["setCookieDomain", "*.thomas-henon.fr https"]);
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-    var u="//analytics.toune.fr/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', '1']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-})();
+ var u="//analytics.toune.fr/";
+ _paq.push(['setTrackerUrl', u+'piwik.php']);
+ _paq.push(['setSiteId', '1']);
+ var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+ g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+ })();
