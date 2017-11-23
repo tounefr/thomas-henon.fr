b291f52ccf6d553f6aaa83d8ee2b860de5caaf65
Thomas HENON <thomas.henon@epitech.eu>
2017-11-23 14:04:25 +0100

    admin matcher

--- app/src/AppBundle/Profiler/AdminMatcher.php
+++ app/src/AppBundle/Profiler/AdminMatcher.php
@@ -1,7 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: toune
- * Date: 14/11/17
- * Time: 16:36
- */
+namespace AppBundle\Profiler;
+
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestMatcherInterface;
+
+class AdminMatcher implements RequestMatcherInterface
+{
+    protected $authorizationChecker;
+
+    public function __construct(AuthorizationCheckerInterface $authorizationChecker)
+    {
+        $this->authorizationChecker = $authorizationChecker;
+    }
+
+    public function matches(Request $request)
+    {
+        return $this->authorizationChecker->isGranted('ROLE_ADMIN');
+    }
+}
